ebfb4ca000185f2ce48824d6858b9c7f5e41896e
csidei <ideic@freemail.hu>
2015-12-15 21:51:49 +0100

    add timer

--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
 (function(){
-	var tempController = {
+	var tempData = {
 		maxValue:50,
 		minValue:0,
 		currentValue:10,
 		alarm:true
 	};
 	var app = angular.module('mcuTest', []);
-	app.controller("TempController", function(){
-		this.temp = tempController;
+	app.controller("TempController", function($http,$interval){
+		this.temp = tempData;
+		$http.get("/getTemp").success(function(data){                           
+                            this.temp=data;                                
+                        });
+		var timer = $interval( function(){
+                    $http.get("/getTemp").success(function(data){                           
+                            this.temp=data;                                
+                        }
+                    );
+                }, 3000);
 	});	
+	
 })();

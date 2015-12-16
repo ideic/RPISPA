6544b0c407d16a892564d88d44d2cec3988c5959
csidei <ideic@freemail.hu>
2015-12-16 22:03:31 +0100

    poller get works

--- public/app.js
+++ public/app.js
@@ -1,22 +1,32 @@
 (function(){
 	var tempData = {
 		maxValue:50,
-		minValue:0,
+		minValue:5,
 		currentValue:10,
-		alarm:true
+		alarm:false
 	};
 	var app = angular.module('mcuTest', []);
-	app.controller("TempController", function($http,$interval){
-		this.temp = tempData;
-		$http.get("/getTemp").success(function(data){                           
-                            this.temp=data;                                
+	app.controller("TempController", function(pollingService) {
+		this.temp = pollingService.result;
+		this.setTemp = function(ctrl){
+			$http.post("setTemp", ctrl.temp).success(function(resultData){
+				alert(resultData);
+			});
+		};
+	});
+
+	app.factory("pollingService", function($http, $timeout){
+		var data = tempData;
+		var poller = function(){
+			$http.get('/getTemp').then(function(response){
+                            data.maxValue=response.data.maxValue;
+                            data.minValue=response.data.minValue;
+                            data.currentValue=response.data.currentValue;
+							data.alarm=response.data.alarm;
+							$timeout(poller, 10000);
                         });
-		var timer = $interval( function(){
-                    $http.get("/getTemp").success(function(data){                           
-                            this.temp=data;                                
-                        }
-                    );
-                }, 3000);
-	});	
-	
+		};
+		poller();
+		return {result: data};
+	});
 })();
